549cced8ba3f4fcf8836a0f356c474105c20e94f
jake trower <cakeandturtles@gmail.com>
2016-09-27 22:00:59 -0700

    adding speak

--- main.js
+++ main.js
@@ -43,7 +43,7 @@ function initCanvas() {
     ctx = canvas.getContext("2d");
 }
 
-function initGame(){    
+function initGame() {    
     dog = new Dog();
     dog.x = 128;
     dog.y = 128;
@@ -55,7 +55,7 @@ function initGame(){
     frog.image = Resources.getImage(FROG_IMG);
     frog.animation.change(0, 0, 4);
     
-    window.setInterval(function(){
+    window.setInterval(function() {
         update();
         render();
     }, 33);
@@ -65,7 +65,7 @@ function initGame(){
     window.onkeyup = Input.onkeyup;
 }
 
-function update(){
+function update() {
     dog.update();
     frog.update();
     
@@ -73,10 +73,12 @@ function update(){
     Input.update();
 }
 
-function render(){
-    ctx.fillStyle = "#F36B72";
+function render() {
+	var text_color = "#FFFFFF";
+	var bg_color = "#F36B72";
+    ctx.fillStyle = bg_color;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    
+ 
     if (dog.y > frog.y + 8){
         frog.render();
         dog.render();
@@ -84,5 +86,38 @@ function render(){
         dog.render();
         frog.render();
     }
+	
+	if (((dog.x >= frog.x && dog.x <= frog.x + frog.animation.frame_width) 
+		|| (dog.x + dog.animation.frame_width >= frog.x 
+			&& dog.x + dog.animation.frame_width <= frog.x + frog.animation.frame_width))
+		&& ((dog.y >= frog.y && dog.y <= frog.y + frog.animation.frame_height)
+		|| (dog.y + dog.animation.frame_height >= frog.y 
+			&& dog.y + dog.animation.frame_height <= frog.y + frog.animation.frame_height))) 
+	{
+		speak("hello lil doggy\nhow are you doing?", text_color, bg_color);
+	}
 }
 
+// TODO(jakeonaut): allow horizontal or vertical alignment (left/right and top/bottom)
+//	will need to reverse for loop for top alignment
+//	need to use measureText width to adjust position of left position on fillText
+function speak(text, text_color, bg_color) {
+	var line_height = 16;
+	
+	ctx.font = line_height + "px Verdana";
+	
+	var y_offset = 64 - line_height*2;
+	var y_kerning = 4;
+	
+	var lines = text.split("\n");
+	for (var i = lines.length-1; i >= 0; i--) {
+		var line = lines[i];
+		
+		ctx.fillStyle = "#000000";
+		ctx.fillRect(16 - y_kerning, y_offset - y_kerning, 
+			ctx.measureText(line).width + y_kerning * 2, 16 + y_kerning * 2);
+		ctx.fillStyle = text_color;
+		ctx.fillText(line, 16, y_offset + 14);
+		y_offset -= (line_height + y_kerning);
+	}
+}
